5299d761649f1d177b4a844636bd4c9fbe48815a
AJAY BABU <42473536+ajay0023130@users.noreply.github.com>
2023-10-12 08:31:18 +0000

    let const var

---
+++ basic/varibale.js
@@ -0,0 +1,9 @@
+const accountId = 321
+let emailId = "ajay@gmail.com"
+var accountPassword = "1234"
+accoutCity = "jaipur"
+// accountId= 543 // can not change const value
+// var not prefer for declartions
+console.log(accountId);
+let accountState;
+console.table([emailId,accountId,accountPassword,accoutCity,accountState])
