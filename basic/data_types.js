48b6a344c39f3d3b1ef926e07df34b4d5a2a8ab6
AJAY BABU <42473536+ajay0023130@users.noreply.github.com>
2023-10-12 11:12:37 +0000

    data type

---
+++ basic/data_types.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all code to new version code
+
+let name ="ajay"
+let age = 10
+let salary = 10.0
+
+console.log(typeof salary)
+
+// data types
+
+// Number => number value
+// BigInt
+// String=> ""
+// Boolean 
+// undefined = 
+// null => standlone value
+// Symbol => for unique
+
+
+console.log(typeof null) // object type
+
+console.log(typeof undefined)
