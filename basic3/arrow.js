9bf896e034384c30a5e31c0ae0266fecd5792aa6
AJAY BABU <42473536+ajay0023130@users.noreply.github.com>
2023-10-13 14:18:28 +0000

    basic

---
+++ basic3/arrow.js
@@ -0,0 +1,66 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// const chai =  () => {
+//     let username = "hitesh"
+//     console.log(this);
+//     console.log(this.username);
+// }
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+// Immediately Invoked Function Expressions
+//iife (immi)
+
+// function chai(){
+//     console.log("this is chai fucntion")
+// }
+// chai()
+
+// global scope ke polution se problem ho na isliye iffe use karte
+
+
+
+(function chai (){ 
+    console.log("this is iffe function")
+    })();
+
