32f86f63c4a360fc6552fe1a31546684a46cfc44
AJAY BABU <42473536+ajay0023130@users.noreply.github.com>
2023-10-13 11:37:39 +0000

    array

---
+++ basic2/array.js
@@ -0,0 +1,26 @@
+const myArr1 = [1,2,3,4,5,6]
+// console.log(myArr1)
+
+const myArr2 = new Array(1,2,3,4)
+
+// console.log(myArr2)
+// console.log(typeof myArr2)
+
+// array method 
+const myArr = [1,2,3,4,5]
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr.includes(9))
+// console.log(myArr)
+
+//slice ,splice
+console.log("A",myArr)
+const myn1 = myArr.slice(1,3)
+console.log(myn1)
+
+const myn2 = myArr.slice(1,3)
+console.log("A",myArr)
+console.log(myn2)
+
+
